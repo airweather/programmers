4ce1fba9776a7e53647a40fa80951f2dae32e301
Jun <wnsghd1@naver.com>
2023-10-15 01:45:31 +0900

    문자열 내 마음대로 정리하기

--- level1/문자열 내 마음대로 정렬하기.js
+++ level1/문자열 내 마음대로 정렬하기.js
@@ -1,25 +1,39 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12915
 // 문자열 내 마음대로 정렬하기
 
-function solution(strings, n) {
+//function solution(strings, n) {
     
-    for(let i=0;i<strings.length;i++) {
-        for(let j=0; j< strings.length-1; j++) {
-            if(strings[j][n] === strings[j+1][n] && strings[j] > strings[j+1]) {
-                const temp = strings[j+1];
-                strings[j+1] = strings[j];
-                strings[j] = temp;
-            }
+//    for(let i=0;i<strings.length;i++) {
+//        for(let j=0; j< strings.length-1; j++) {
+//            if(strings[j][n] === strings[j+1][n] && strings[j] > strings[j+1]) {
+//                const temp = strings[j+1];
+//                strings[j+1] = strings[j];
+//                strings[j] = temp;
+//            }
             
-            if(strings[j][n] > strings[j+1][n]) {
-                const temp = strings[j+1];
-                strings[j+1] = strings[j];
-                strings[j] = temp;
-            }
-        }
+//            if(strings[j][n] > strings[j+1][n]) {
+//                const temp = strings[j+1];
+//                strings[j+1] = strings[j];
+//                strings[j] = temp;
+//            }
+//        }
+//    }
+    
+    
+    
+//    return strings;
+//}
+
+function solution(strings, n) {
+  return strings.sort((a, b) => {
+    // 주어진 인덱스(n)에 있는 문자를 비교
+    const compareByN = a[n].localeCompare(b[n]);
+    
+    if (compareByN === 0) {
+      // 주어진 인덱스의 문자가 같은 경우, 사전적으로 비교
+      return a.localeCompare(b);
     }
     
-    
-    
-    return strings;
+    return compareByN;
+  });
 }
