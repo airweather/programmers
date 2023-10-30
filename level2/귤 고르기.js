b622f39334c501ed5d21b636aa0874fcc44ff644
Jun <wnsghd1@naver.com>
2023-10-31 02:42:50 +0900

    귤 고르기

---
+++ level2/귤 고르기.js
@@ -0,0 +1,27 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/138476
+// 귤 고르기
+
+function solution(k, tangerine) {
+    
+    const counter = (arr) => {
+        const obj = {};
+        
+        arr.map((item) => {
+            
+            obj[item] ? obj[item] += 1 : obj[item] = 1
+                        
+        });
+        
+        return obj;
+    };
+    
+    const counted = Object.entries(counter(tangerine)).sort((a, b) => b[1] - a[1]);
+    
+    let i = 0;
+    while(k > 0) {
+        k -= counted[i][1];
+        i++;
+    }
+    
+    return i;
+}
